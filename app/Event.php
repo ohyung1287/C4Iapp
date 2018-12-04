caeef29b38b39f235f42dd2d93c5278ca8b7810b
ZHENGXU LIU <37124277+vanshg521@users.noreply.github.com>
2018-12-04 18:27:53 -0500

    Event, Lending Models

---
+++ app/Event.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    //
+    protected $table = 'events';
+
+    public $primaryKey = 'id';
+
+    public $timestamps = true;
+}
